94466a732d9b8edda806d3c072d62364c5737d86
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2020-12-15 14:14:40 +0100

    feat: Add language dropdown

---
+++ src/content_scripts/dropdown.ts
@@ -0,0 +1,41 @@
+interface DropdownSettings<T> {
+	items: readonly T[];
+	display(item: T): string;
+	onChange(value: T): void;
+}
+
+export class Dropdown<T> {
+	element: HTMLSelectElement;
+	private items: DropdownSettings<T>["items"];
+
+	constructor({ items, display, onChange }: DropdownSettings<T>) {
+		this.items = items;
+		this.element = document.createElement("select");
+
+		this.items
+			.map((x, i) => {
+				const option = document.createElement("option");
+				option.value = String(i);
+				option.textContent = display(x);
+				return option;
+			})
+			.forEach(option => this.element.appendChild(option));
+
+		this.element.addEventListener("change", () => {
+			onChange(this.selected());
+		});
+	}
+
+	selected(): T {
+		const selectedOption = this.element.options[this.element.selectedIndex];
+		const selectedIndex = Number(selectedOption.value);
+		return this.items[selectedIndex];
+	}
+
+	select(predicate: (x: T) => boolean): void {
+		const index = this.items.findIndex(predicate);
+		if (index > -1) {
+			this.element.selectedIndex = index;
+		}
+	}
+}
--- src/content_scripts/index.ts
+++ src/content_scripts/index.ts
@@ -1,4 +1,10 @@
 import Prism from "../vendor/prism";
+import { Dropdown } from "./dropdown";
+import {
+	languages as allLanguages,
+	PrismLanguage,
+	Language,
+} from "./languages";
 
 function assert<T>(x: T): asserts x {
 	if (!x) {
@@ -6,45 +12,114 @@ function assert<T>(x: T): asserts x {
 	}
 }
 
+class CodeBlock {
+	pre: HTMLElement;
+	code: HTMLElement;
+	private lang: PrismLanguage;
+
+	constructor(pre: HTMLElement, code: HTMLElement) {
+		this.pre = pre;
+		this.code = code;
+		this.lang = this.code.getAttribute("data-rch-language") ?? "none";
+	}
+
+	get visited() {
+		return this.code.hasAttribute("data-rch-visited");
+	}
+
+	set visited(value: boolean) {
+		if (value) {
+			this.code.setAttribute("data-rch-visited", "true");
+		} else {
+			this.code.removeAttribute("data-rch-visited");
+		}
+	}
+
+	private get language() {
+		return this.lang;
+	}
+
+	private set language(lang: PrismLanguage) {
+		this.code.classList.remove("language-" + this.lang);
+		this.pre.classList.remove("language-" + this.lang);
+		this.code.classList.add("language-" + lang);
+		this.code.setAttribute("data-rch-language", lang);
+		this.lang = lang;
+	}
+
+	highlight(lang: PrismLanguage = this.lang) {
+		this.code.classList.add("line-numbers");
+		this.language = lang;
+		Prism.highlightElement(this.code);
+
+		/*
+		 * HACK:
+		 * Reddit's CSS sets the parent <pre> element's display to grid.
+		 * This interferes with the Line Numbers plugin and makes the numbering
+		 * completely invisible. Removing the grid display makes the numbering
+		 * appear, but it isn't aligned properly to the lines of code. For some
+		 * reason, setting the display to "table" on the <pre> element itself
+		 * and making sure that the line-height is set to 1 fixes the invisible line
+		 * numbers and aligns them to the code properly.
+		 */
+		this.pre.style.display = "table";
+		this.pre.style.lineHeight = "1";
+		this.pre.style.width = "100%";
+	}
+
+	rehighlight(lang: PrismLanguage) {
+		if (lang === this.language) {
+			return;
+		}
+
+		this.language = lang;
+		const content = this.code.textContent;
+		this.code.innerHTML = "";
+		this.code.textContent = content;
+		Prism.highlightElement(this.code);
+	}
+}
+
 const qsa = (selector: string, parent: ParentNode = document) =>
 	Array.from(parent.querySelectorAll(selector));
-
-function isVisited(el: HTMLElement) {
-	return el.hasAttribute("data-rch-visited");
-}
-
-function visit(el: HTMLElement) {
-	el.setAttribute("data-rch-visited", "true");
-}
 
 function getCodeBlocks() {
 	return qsa("pre code").map(code => {
 		const pre = code.parentElement;
 		assert(pre);
-		return { pre, code: code as HTMLElement };
+		return new CodeBlock(pre, code as HTMLElement);
 	});
 }
 
-setInterval(() => {
-	getCodeBlocks().forEach(({ pre, code }) => {
-		if (!isVisited(code) || code.firstChild?.nodeType === Node.TEXT_NODE) {
-			code.classList.add("line-numbers", "language-javascript");
-			Prism.highlightElement(code);
+function sortLanguages(a: Language, b: Language) {
+	if (a.lang === "none") return -1;
+	if (b.lang === "none") return 1;
+	return a.name.localeCompare(b.name);
+}
 
-			/*
-			 * HACK:
-			 * Reddit's CSS sets the parent <pre> element's display to grid.
-			 * This interferes with the Line Numbers plugin and makes the numbering
-			 * completely invisible. Removing the grid display makes the numbering
-			 * appear, but it isn't aligned properly to the lines of code. For some
-			 * reason, setting the display to "table" on the <pre> element itself
-			 * and making sure that the line-height is set to 1 fixes the invisible line
-			 * numbers and aligns them to the code properly.
-			 */
-			pre.style.display = "table";
-			pre.style.lineHeight = "1";
+function insertPicker(codeBlock: CodeBlock) {
+	const wrapper = document.createElement("div");
+	wrapper.classList.add("rch-picker");
 
-			visit(code);
-		}
+	const items = allLanguages.slice().sort(sortLanguages);
+
+	const drp = new Dropdown({
+		items,
+		display: x => x.name,
+		onChange: x => codeBlock.rehighlight(x.lang),
 	});
-}, 1000);
+
+	codeBlock.pre.insertAdjacentElement("beforebegin", drp.element);
+}
+
+function setup() {
+	for (const block of getCodeBlocks()) {
+		if (!block.visited) {
+			insertPicker(block);
+			block.highlight();
+			block.visited = true;
+		}
+	}
+}
+
+setInterval(setup, 2000);
